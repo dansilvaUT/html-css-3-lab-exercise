7f14b435214401bcfaa7ab97a50024caf9255feb
dansilvaUT <dan13silv@gmail.com>
2021-01-07 15:00:10 -0800

    Finished working on some additional practice

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      toggleView: false
+    }
+  }
+
+  toggleMenu = () => {
+    this.setState({ toggleView: !this.state.toggleView })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <section className="landing">
+          <header className="navbar">
+            <h4>Start Bootstrap</h4>
+            <nav className="nav-list">
+              <span>Services</span>
+              <span>Portfolio</span>
+              <span>About</span>
+              <span>Team</span>
+              <span>Contact</span>
+            </nav>
+            <div className="dropdown" onClick={this.toggleMenu}>Menu</div>
+            {this.state.toggleView
+              ? (
+                <nav className="dropdown-menu">
+                  <span>Services</span>
+                  <span>Portfolio</span>
+                  <span>About</span>
+                  <span>Team</span>
+                  <span>Contact</span>
+                </nav>
+              )
+              : null}
+          </header>
+          <section className="landing-middle">
+            <h3>Welcome to Our Studio!</h3>
+            <h1> It's nice to meet you</h1>
+            <button>Tell me more</button>
+          </section>
+        </section>
+      </div>
+    );
+  }
+
 }
 
 export default App;
